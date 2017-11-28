425f599ec6102ddf2b8a0bf553b12b196bb4f779
matijamikac <mmikac@gmail.com>
2017-11-28 21:20:54 +0100

    2

--- murra/settings.py
+++ murra/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-#from urllib import parse
+from urllib import parse
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
