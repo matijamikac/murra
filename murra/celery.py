b5cb5729c41508bb006cebd535548665853e8e5f
matijamikac <mmikac@gmail.com>
2017-12-01 23:39:15 +0100

    task

--- murra/celery.py
+++ murra/celery.py
@@ -22,3 +22,35 @@ def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
 
+from app.app_settings import *
+from app.models import *
+
+
+@app.task(bind=True)
+def depreciation():
+    list_of_positive_balances = PositiveBalance.objects.filter(charge__gt=0)
+
+    for i in list_of_positive_balances:
+    	if i.depreciation < 1:
+	    	new_balance = Balance()
+	        new_balance.value = i.charge * depreciation_provision
+	        new_balance.user_id = i.user_id
+	        new_balance.type = 5
+	        #new_balance.offer_id = offer.id
+	        new_balance.save()
+	        i.transactions.add(new_balance)
+	        i.charge -= new_balance.value
+
+	        total_balance = TotalBalance.objects.get(user_id=i.user_id)
+	        total_balance.value -= new_balance.value
+	        total_balance.save()
+
+	        murra = PositiveBalance()
+	        murra.value = new_balance.value
+	        murra.user_id = provision_receiver_id
+	        murra.charge = new_balance.value
+	        murra.positivebalance_type = 4
+	        murra.save()
+	        murra_totalbalance =TotalBalance.objects.get(user_id=provision_receiver_id)
+	        murra_totalbalance.value += murra.value
+	        murra_totalbalance.save()
